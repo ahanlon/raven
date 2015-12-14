15882876a47d9974a7e44b5529a4d2f437ac71d4
Andrew Hanlon <hanlonad27@gmail.com>
2015-12-14 18:10:28 -0500

    added footer, main div to viewport size, draft formatted for about page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,40 +2,15 @@ $( function() {
 
 console.log('Where my Ravens at??');
 
-// var contactEmail = function(inputName, inputEmail, inputMessage, inputReal) {
-// 	this.inputName		= inputName;
-// 	this.inputEmail		= inputEmail;
-// 	this.inputMessage	= inputMessage;
-// 	this.inputReal 		= inputReal;
-// };
+// making sure the main content on each page expands to the viewport
 
-// var emailData = [];
+var vHeight = $(window).height(),
+    vWidth = $(window).width(),
+    all = $('.all'),
+    about = $('.about');
 
-// $('#submit').on('submit', function() {
-
-
-// 	var inputName = $('#inputName').val();
-// 	var inputEmail = $('#inputEmail').val();
-// 	var inputMessage = $('#inputMessage').val();
-// 	var inputReal = $('#inputReal').val();
-
-// 	var emailData = new contactEmail(inputName, inputEmail, inputMessage, inputReal);
-
-// 	console.log(emailData);
-
-// 	$.ajax({
-// 		method: 'POST',
-// 		url: 'http://formmail.dreamhost.com/cgi-bin/formmail.cgi',
-// 		data: 'emailData',
-// 		error: function(data) {
-// 			$('alert-danger').removeClass('hidden');
-// 		},
-// 		sucess: function(data) {
-// 			$('.alert-success').removeClass('hidden');
-// 		},
-// 	});
-// });
-
+all.css({"height":vHeight,"width":vWidth});
+about.css({"height":vHeight,"width":vWidth});
 
 
 
